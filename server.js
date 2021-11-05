5a3604475379d7d658ac2bd006d97054d97ab83f
inmarluna <inmarluna@Inmars-MacBook-Pro.local>
2021-11-04 21:52:43 -0500

    front end pages

--- server.js
+++ server.js
@@ -10,6 +10,8 @@ const app = express();
 app.use(express.urlencoded({ extended: true }));
 // parse incoming JSON data
 app.use(express.json());
+// routes to everything in our public dir
+app.use(express.static('public'));
 
 function filterByQuery(query, animalsArray) {
     let personalityTraitsArray = [];
@@ -101,6 +103,22 @@ app.get('/api/animals/:id', (req, res) => {
         res.send(404);
     }
   });
+
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
+
+app.get('/animals', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/animals.html'));
+});
+
+app.get('/zookeepers', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/zookeepers.html'));
+});
+
+app.get('*', (req, res) => {
+    res.sendFile(path.join(__dirname, './public/index.html'));
+});
 
 app.post('/api/animals', (req, res) => {
     // set id based on what the next index of the array will be
